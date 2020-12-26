40cd592cb8d44145406b431e3323598859e1b6f1
zukucker <alexander.froehling@googlemail.com>
2020-12-26 12:32:05 +0100

    working delete funciton of lines

--- open.lua
+++ open.lua
@@ -1,5 +1,26 @@
-f = io.input('testfile')
-for line in f:lines() do 
-  print (line)
+function read()
+  content = {}
+  f = io.open('testfile', "r")
+  for line in f:lines() do
+    print(line)
+    content[#content+1] = line
+    remove()
+  end
 end
-f:close()
+
+function remove()
+  for i = 1, #content do
+    f = io.open('testfile', "w+")
+      print(content[i], " wurde gelöscht")
+      f:write("", "")
+    f:close()
+  end
+  sleep(2)
+end
+
+local clock = os.clock
+function sleep(n)  -- seconds
+  local t0 = clock()
+  while clock() - t0 <= n do end
+end
+read()
